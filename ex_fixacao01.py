4aa1b34157adea93c1cea7ff6aceb28743043f0f
jhowzxm <Jhonatan Oliveira>
2023-08-01 11:03:54 -0300

    commit exercicio

---
+++ ex_fixacao01.py
@@ -0,0 +1,10 @@
+'''
+Fazer um programa que pergunte um valor em Dólares e converta em reais. considere que 1 Dólar vale 4,78 reais.
+'''
+
+nar = float(input("Me diga em Dólar um número que deseja converter em Reais:"))
+rs = mar * 4,78
+
+print("O número", mar, "em Dólar é:"),rs,"em Reais")
+
+
